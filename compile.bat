828789734b657448190e2f2a0d345ce92ac22a79
WolfProg7 <maildoguifigueiredo@gmail.com>
2025-12-29 23:31:15 +0000

    v1.1.1 Update (Bug fix)

--- compile.bat
+++ compile.bat
@@ -1,6 +1,13 @@
 @echo off
-gcc wdd.c -o wdd.exe -std=c11 -O2 -Wall -Wextra -Wpedantic ^
-    -Wno-unused-parameter -DWIN32_LEAN_AND_MEAN -municode -static-libgcc -static-libstdc++ -s
+gcc wdd.c ^
+  -o wdd.exe ^
+  -std=c11 ^
+  -O2 ^
+  -Wall -Wextra -Wpedantic ^
+  -DWIN32_LEAN_AND_MEAN ^
+  -municode ^
+  -static-libgcc ^
+  -s
 
 if errorlevel 1 (
     echo Build failed
